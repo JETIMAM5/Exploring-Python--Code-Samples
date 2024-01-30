e9b2f45bda7217b94745b43bbc8e6cfc22d5ebb9
BERAT YETİŞ <127851941+JETIMAM5@users.noreply.github.com>
2024-01-30 18:09:11 +0300

    Add files via upload

---
+++ refernce.py
@@ -0,0 +1,16 @@
+# Value Types => string , integer , float ...
+x = 5
+y = 25
+
+x = y 
+y = 10 
+print(x,y)
+
+# Reference Types => list, class ....
+a = ["Apple","Banana"]
+b = ["Apple","Banana"]
+
+a = b 
+b[0] = "Grape"
+print(a,b)
+ 
