94c4c02a6a2512d8bfda70e06d859cd5f7532bea
BERAT YETİŞ <127851941+JETIMAM5@users.noreply.github.com>
2024-01-30 18:09:39 +0300

    Add files via upload

---
+++ strings.py
@@ -0,0 +1,11 @@
+name = 'Berat'
+surname= 'Yetiş'
+age =  20
+greeting = "My name is "+ name +' '+ surname + ' and \nI am ' + str(age) + ' years old.'
+lenght = len(greeting)
+print(greeting)
+print(greeting[lenght-1])
+print(greeting[3:7])
+print(greeting[3:])
+print(greeting[:16])
+print(greeting[2:40:2])
