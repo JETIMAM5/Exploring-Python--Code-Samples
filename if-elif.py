bea773bcf1957c74af42db0a638d35d6a6648b85
BERAT YETİŞ <127851941+JETIMAM5@users.noreply.github.com>
2024-02-02 20:48:49 +0300

    Add files via upload

---
+++ if-elif.py
@@ -0,0 +1,8 @@
+x = int(input("X : ")) 
+y = int(input("Y : ")) 
+if x > y :
+    print("X is greater thay Y...")
+elif x == y:    
+    print("X and Y are equal...")
+else:
+    print("Y is greater Than X...")        
