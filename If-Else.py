7da6ab4e92ff15f9ebc4fe9ba5125d7f2665efbe
BERAT YETİŞ <127851941+JETIMAM5@users.noreply.github.com>
2024-02-02 20:31:18 +0300

    Add files via upload

---
+++ If-Else.py
@@ -0,0 +1,10 @@
+username = "BeratYetis"
+password = "123456"
+
+if (username == "BeratYetiss"):
+    if(password == "123456"):  
+        print("Welcome")
+    else:
+        print("Incorrect Password...")    
+else:
+    print("Incorrect username...")    
