60a02094d65168ea6aa2b9fd08c5e3dd08243e9f
BERAT YETİŞ <127851941+JETIMAM5@users.noreply.github.com>
2024-02-06 22:02:13 +0300

    Add files via upload

---
+++ ForLoop.py
@@ -0,0 +1,24 @@
+#numbers = [1,2,3,4,5,6]
+
+#for num in numbers:
+#    print(num)
+
+#-----------------------
+
+#ames = ["Berat","Esma","Ayşe","Fatma"]
+#for name in names:
+#     print(f"My name is {name}")
+
+#name ="Berat Yetis"
+#for n in name:
+#    print(n)
+
+#tuple = [(1,2),(1,3),(3,5),(5,7)]   
+
+#for a,b in tuple:
+#    print(a)
+
+d = {"k1":1,"k2":2,"k3":3}
+for item in d.items() :
+    print(item)
+#prints  just keys
