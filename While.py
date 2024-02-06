62908be1c885554a82048629580fa8a639037215
JETIMAM <127851941+JETIMAM5@users.noreply.github.com>
2024-02-06 23:47:29 +0300

    While_Loop

---
+++ While.py
@@ -0,0 +1,15 @@
+# From 1 to 100
+
+#x = 0
+#while x <= 100:
+#    if (x%2==0):
+#      print(f"Even number : {x}")
+#    else:
+#       print(f"Odd Number : {x}")  
+#    x += 1
+
+name = ''
+while not name.strip():
+    name=(input("Enter Your Name:\n"))
+
+print(f"Hello , {name}")
