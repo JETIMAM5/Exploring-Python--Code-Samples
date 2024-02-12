ef364edecb314f995598fbe74d2f790b008a57a3
JETIMAM <127851941+JETIMAM5@users.noreply.github.com>
2024-02-12 18:40:44 +0300

    Small differences between functions and methods

---
+++ methods.py
@@ -0,0 +1,14 @@
+#methods
+
+list = [1,2,3]
+list.append(4) # --> adds 4 to end of list  
+list.pop()     # --> removes the last list member
+print(type(list)) # --> class 'list'
+print(list)
+
+myString = "Hello"  
+print(myString.upper()) # --> HELLO 
+print(type(myString)) # --> class 'string'
+
+#functions  (we're not define functions in classes) do whatever you want to do with it (according to your implementation) 
+# we don't need any class name and a dot(.) to reach a function 
