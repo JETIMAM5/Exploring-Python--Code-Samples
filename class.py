3d07a808a394b7832b97cedca2d5cd6ee2270a60
JETIMAM <127851941+JETIMAM5@users.noreply.github.com>
2024-02-14 20:09:09 +0300

    Added Class 'Circle'

--- class.py
+++ class.py
@@ -1,5 +1,4 @@
 # Class
-
 class Person:
     
     #  Class attributes
@@ -7,38 +6,86 @@ class Person:
 
 
     #  constructor(constrcutor method)
-    #  These are object attributes
+    
     def __init__(self,name,year):
+
+        # object attributes
         self.name = name
         self.year = year
-        print("init worked")
+        
 
-    
-    
-    # Methods
+    # Instance Method    
+    def intro(self):
+        print("Hello There . I am "+ self.name)
+
+    # Instance Method  
+    def calculateAge(self):
+        age = 2024 - self.year
+        return age
 
 
-
+#-----------------------------------------------------------------------------------------------------------------
+#-----------------------------------------------------------------------------------------------------------------
 
 # Object (Instance)
 
 p1=Person(name="Ali",year=1990) #First way
 p2=Person("Berat",2004)         #Second way to define an instance
 
+# Calling method
+p1.intro()
+print(f"I am {p1.calculateAge()} years old")
+p2.intro()
+print(f"I am {p2.calculateAge()} years old")
 # Updating 
-p1.name = "Salim"
-p1.address = "İstanbul"
+#p1.name = "Salim"
+#p1.address = "İstanbul"
 
 # Accesing object attributes 
-print(f" p1 Name = {p1.name} , Year = {p1.year} , Address = {p1.address}") 
-print(f" p2 Name = {p2.name} , Year = {p2.year} , Address = {p2.address}")
+#print(f" p1 Name = {p1.name} , Year = {p1.year} , Address = {p1.address}") 
+#print(f" p2 Name = {p2.name} , Year = {p2.year} , Address = {p2.address}")
 
-print(p1)
-print(p2)
 
+#-----------------------------------------------------------------------------------------------------------------
+#-----------------------------------------------------------------------------------------------------------------
+#-----------------------------------------------------------------------------------------------------------------
+#-----------------------------------------------------------------------------------------------------------------
+#print(p2)
+#-----------------------------------------------------------------------------------------------------------------
+#-----------------------------------------------------------------------------------------------------------------
 #Type of instances will be 'Person' and also they have differenet RAM addresses
-print(type(p1))
-print(type(p2))
-
+#print(type(p1))
+#print(type(p2))
+#-----------------------------------------------------------------------------------------------------------------
+#-----------------------------------------------------------------------------------------------------------------
 # Instances are not same , because they have different RAM addresses
-print(p1==p2)
+#print(p1==p2)
+#-----------------------------------------------------------------------------------------------------------------
+#-----------------------------------------------------------------------------------------------------------------
+
+#-----------------------------------------------------------------------------------------------------------------
+#-----------------------------------------------------------------------------------------------------------------
+
+#-----------------------------------------------------------------------------------------------------------------
+#-----------------------------------------------------------------------------------------------------------------
+print("-----------------------------------------------------------------------------------------------------------")
+class Circle:
+    # Class attribute 
+    pi = 3.14
+
+    #Constructor
+    def __init__(self,radius=1):
+        self.radius = radius
+
+    # Instance Method
+    def calculate_perimeter(self):
+        return (2*(self.pi)*(self.radius))
+    
+    def calculate_area(self):
+        return (self.pi*(self.radius**2))
+        
+c1 = Circle() # radius = 1
+c2 = Circle(3) # radius = 3
+
+print(f"Area of c1 is {c1.calculate_area()} , Periemeter of c1 is {c1.calculate_perimeter()}")
+print(f"Area of c2 is {c2.calculate_area()} , Periemeter of c2 is {c2.calculate_perimeter()}")
