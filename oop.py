7c4f8e2252dff17e8dee94eb2ef57e3606fa78b8
JETIMAM <127851941+JETIMAM5@users.noreply.github.com>
2024-02-14 18:17:30 +0300

    What Is OOP ?

---
+++ oop.py
@@ -0,0 +1,10 @@
+# Object Orianted Programming (OOP) 
+
+# Class ---- We have classes that already defined in library , also we can create our own classes 
+# Class Person : name , surname , birthday , calculateAge() , 
+# Instance(object) 
+lst=[1,2,3] # --> This is an instance of 'list' class 
+
+result = type(lst)
+print(result)
+
