d23e946dde358a40a89bf57262e9eebadb6635a6
JETIMAM <127851941+JETIMAM5@users.noreply.github.com>
2024-02-18 20:25:33 +0300

    What is Eroor &  basic error types

---
+++ errors.py
@@ -0,0 +1,6 @@
+# error
+#        print(a) --->> NameError
+#        int("1a2") --->> ValeuError
+#        print(10/0) --->> ZeroDivisionError
+#        print("denem"e) ---> SyntaxError
+# More Degails on page python.org 
