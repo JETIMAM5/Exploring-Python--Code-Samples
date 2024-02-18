e415c9caf3282b17b2cc9bd855455a4da66e87f4
JETIMAM <127851941+JETIMAM5@users.noreply.github.com>
2024-02-18 20:01:41 +0300

    How to import a module ? Paths of modules and DLLs

---
+++ mod-module.py
@@ -0,0 +1,16 @@
+import mod
+import math
+
+# result = help(mod)
+# result = help(mod.func)
+# result = mod.number
+# result = mod.numbers
+# result = mod.person["Age"]
+# result = mod.person["name"]
+# result = mod.person["city"]
+result = mod.func(10)
+
+p = mod.Person()
+p.speak()
+
+#print(result)
---
+++ mod.py
@@ -0,0 +1,22 @@
+'''
+About this module ..........
+'''
+print("Added module")
+number = 10
+numbers = [1,2,3]
+
+person={
+    "name" : "Ali",
+    "Age"  :  "25",
+    "city" : "İstanbul"
+}
+
+def func(x):
+    '''
+    About this function ........
+    '''
+    print(f"x: " + str(x))
+
+class Person:
+    def speak(self):
+        print("I am speaking now....")  
