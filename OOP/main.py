461e4eabb0708d7adcd940470019de9af14b0f14
BERAT YETİŞ <127851941+JETIMAM5@users.noreply.github.com>
2024-08-23 14:09:33 +0300

    CoffeeMachineWithOOP
    This is an imitation of a coffee machine (OOP included)

---
+++ OOP/main.py
@@ -0,0 +1,44 @@
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+this_menu = Menu()
+this_coffee_maker = CoffeeMaker()
+this_money_machine = MoneyMachine()
+is_on = True
+
+this_coffee_maker.report()
+this_money_machine.report()
+
+
+while is_on:
+    menu = this_menu.get_items()
+    order_name = input(f"What would you like? ({menu}): ").lower()
+
+    if order_name == "off":
+        is_on = False
+    elif order_name == "report":
+        this_coffee_maker.report()
+        this_money_machine.report()
+    else:
+        drink = this_menu.find_drink(order_name)
+        if this_coffee_maker.is_resource_sufficient(drink) and this_money_machine.make_payment(drink.cost):
+            this_coffee_maker.make_coffee(drink)
+        else:
+            pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
