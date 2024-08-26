2f003ab72aa34e53163535dd5d74a661cc22856f
JETIMAM <127851941+JETIMAM5@users.noreply.github.com>
2024-08-26 20:11:51 +0300

    Done . Mail Merge Project -> Aims for file operations , opening a file , replacing a string between files. Also, relative pathways examined

---
+++ FileOperations/MailMergeProject/Mail Merge Project Start/main.py
@@ -0,0 +1,18 @@
+
+with open("Input/Letters/starting_letter.txt", mode="r") as letter_file:
+    letters = letter_file.read()
+    letters.strip("[]")
+
+with open("Input/Names/invited_names.txt", mode="r") as name_file:
+    names = name_file.readlines()
+
+for name in names:
+    name = name.strip()
+    personalized_message = letters.replace("[name]", name)
+    with open(f"Output/ReadyToSend/MessageFor{name}", mode="w") as output_folder:
+        output_folder.write(personalized_message)
+
+print("Done")
+
+
+
