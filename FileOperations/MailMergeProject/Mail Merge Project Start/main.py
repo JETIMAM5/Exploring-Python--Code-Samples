edf4970a91f471355d3a6a3bffc8b5f7a3d47148
JETIMAM <127851941+JETIMAM5@users.noreply.github.com>
2024-08-27 17:59:42 +0300

    Pandas library was introduced, Dataframes and various manipulations were made.

--- FileOperations/MailMergeProject/Mail Merge Project Start/main.py
+++ FileOperations/MailMergeProject/Mail Merge Project Start/main.py
@@ -1,15 +1,16 @@
+PLACEHOLDER = "[name]"
 
-with open("Input/Letters/starting_letter.txt", mode="r") as letter_file:
+with open("./Input/Letters/starting_letter.txt", mode="r") as letter_file:
     letters = letter_file.read()
     letters.strip("[]")
 
-with open("Input/Names/invited_names.txt", mode="r") as name_file:
+with open("./Input/Names/invited_names.txt", mode="r") as name_file:
     names = name_file.readlines()
 
 for name in names:
     name = name.strip()
-    personalized_message = letters.replace("[name]", name)
-    with open(f"Output/ReadyToSend/MessageFor{name}", mode="w") as output_folder:
+    personalized_message = letters.replace(PLACEHOLDER, name)
+    with open(f"./Output/ReadyToSend/MessageFor{name}", mode="w") as output_folder:
         output_folder.write(personalized_message)
 
 print("Done")
---
+++ WorkingWithCSVFilesAndPandasLibrary/main.py
@@ -0,0 +1,61 @@
+#
+# with open("weather_data.csv", mode="r") as data_file:
+#     data = data_file.readlines()
+#     print(data)
+
+
+# import csv
+#
+# with open("weather_data.csv") as data_file:
+#     data = csv.reader(data_file)
+#     temperatures = []
+#     for row in data:
+#         if row[1] != "temp":
+#             temperatures.append(int(row[1]))
+#     print(temperatures)
+
+import pandas
+
+data = pandas.read_csv('weather_data.csv')
+
+# # print(type(data))  # = <class 'pandas.core.frame.DataFrame'> => LIKE EXCEL TABLE
+# print(type(data["temp"]))  # = <class 'pandas.core.series.Series'> => LIKE JUST ONE COLUMN OF AN EXCEL TABLE
+
+# data_dict = data.to_dict()
+# print(data_dict)
+# ---------------------------------------------------------------------------------------------------------------------
+
+# temp_list = data["temp"].to_list()
+# print(f"Length of the list 'Temperatures' : {len(temp_list)}")
+#
+# print(f"Mean of temperatures : {data["temp"].mean().round(2)}")
+#
+# print(f"Maximum value of temperatures : {data['temp'].max()}")
+
+# ---------------------------------------------------------------------------------------------------------------------
+# Getting data[condition] in columns
+# print(data["condition"])
+# Additionally, we can achieve this through that method.
+# print(data.condition)
+# Pandas does the jobs behind the scene, allowing us to manage our data by using the dot notation
+# But you need to be aware of the column name you are using, as this process is case-sensitive.
+
+# ---------------------------------------------------------------------------------------------------------------------
+
+# Getting data in rows
+# print(data[data.temp == data.temp.max()])
+#
+# monday = data[data.day == "Monday"]
+# print(monday.condition)
+# print(f"The Temperature on Monday is {monday["temp"]*9/5 + 32} degrees Fahrenheit")
+
+# --------------------------------------------------------------------------------------------------------------------
+
+# Creating a DataFrame from scratch
+data_dict = {
+    "students": ["Amy", "James", "John"],
+    "scores": [76, 56, 65]
+}
+data_2 = pandas.DataFrame(data_dict)
+print(data_2)
+data_2.to_csv('new_data.csv')
