b44b5e7a6555570b9cdf5ff20930f630d60a4d18
JETIMAM <127851941+JETIMAM5@users.noreply.github.com>
2024-09-04 18:38:18 +0300

    Edited components with grid and place methods , added padding to components and the window

--- Tkinter/main.py
+++ Tkinter/main.py
@@ -1,8 +1,14 @@
 from tkinter import *
 
+
+def button_clicked():
+    my_label.config(text=input0.get())
+
+
 window = Tk()
 window.title('GUI')
 window.minsize(500, 300)
+window.config(padx=20, pady=20)
 
 
 # COMPONENTS
@@ -13,23 +19,26 @@ my_label = Label(text="This is a label", font=("Arial", 24, "bold"))
 # also we need to specify how this component laid out on the screen
 my_label["text"] = "New Text"  # we can change the kwargs like this or like the line below
 my_label.config(text="New Text")
-my_label.pack()  # This pack method is going to center our label
-
+# my_label.place(x=100, y=200) # Specific coordinate of a component
+# my_label.pack()  # This pack method is going to center our label
+my_label.grid(column=0, row=0)
+my_label.config(padx=20, pady=20)
 
 # Button
 
-
-def button_clicked():
-    my_label.config(text=input0.get())
-
-
 button = Button(text="Click Me", command=button_clicked)
-button.pack()
+# button.pack
+button.grid(column=1, row=1)
+#
+# # Entry
+#
 
-# Entry
+button2 = Button(text="New Button", command=button_clicked)
+button2.grid(column=2, row=0)
 
 input0 = Entry(width=10)
-input0.pack()
+# input0.pack()
+input0.grid(column=2, row=3)
 
 
 window.mainloop()
