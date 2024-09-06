8cf22d0361c4439b2463de5028d57248aabbdab7
JETIMAM <127851941+JETIMAM5@users.noreply.github.com>
2024-09-06 20:42:53 +0300

    Refactor Last : Completed Simple Password Generator App

--- Tkinter/PasswordManagerApp/main.py
+++ Tkinter/PasswordManagerApp/main.py
@@ -1,8 +1,27 @@
 from tkinter import *
 from tkinter import messagebox
-# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+from random import choice, randint, shuffle
+import pyperclip
 
-# ---------------------------- SAVE PASSWORD ------------------------------- #
+# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+# Password Generator Project
+
+
+def generate_password():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    password_letters = [choice(letters) for _ in range(randint(8, 10))]
+    password_symbols = [choice(symbols) for _ in range(randint(2, 4))]
+    password_numbers = [choice(numbers) for _ in range(randint(2, 4))]
+
+    password_list = password_letters + password_symbols + password_numbers
+    shuffle(password_list)
+
+    password = "".join(password_list)
+    password_entry.insert(0, password)
+    pyperclip.copy(password)
 
 
 def save():
@@ -37,15 +56,16 @@ password_label.grid(row=3, column=0, sticky="E")
 
 # Entries
 website_entry = Entry(window, width=35)
-email_entry = Entry(window, width=35)
+email_entry = Entry(window, width=35, )
 password_entry = Entry(window, width=21)
-email_entry.grid(row=1, column=1, columnspan=2, sticky="EW")
-website_entry.grid(row=2, column=1, columnspan=2, sticky="EW")
+email_entry.insert(0, "berat@outlook.com")
+website_entry.grid(row=1, column=1, columnspan=2, sticky="EW")
+email_entry.grid(row=2, column=1, columnspan=2, sticky="EW")
 password_entry.grid(row=3, column=1, sticky="EW")
 
 # Buttons
-generate_button = Button(window, text="Generate Password")
-add_button = Button(window, text="Add", width=36)
+generate_button = Button(window, text="Generate Password", command=generate_password)
+add_button = Button(window, text="Add", width=36, command=save)
 generate_button.grid(row=3, column=2, sticky="EW")
 add_button.grid(row=4, column=1, columnspan=2, sticky="EW")
 
