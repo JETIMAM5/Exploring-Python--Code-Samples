7187323a2ab2a6f903995a6be8df38d4717769aa
JETIMAM <127851941+JETIMAM5@users.noreply.github.com>
2024-09-06 22:45:07 +0300

    Handled subject : Raising Custom Error , Keywords of error handling

--- Errors&ErrorHandling/errors.py
+++ Errors&ErrorHandling/errors.py
@@ -1,6 +1,24 @@
 # error
 #        print(a) --->> NameError
-#        int("1a2") --->> ValeuError
+#        int("1a2") --->> ValueError
 #        print(10/0) --->> ZeroDivisionError
 #        print("denem"e) ---> SyntaxError
-# More Degails on page python.org 
+# More Details on page python.org
+
+# FileNotFound
+# with open ("a_file.txt","r") as file:
+#     file.read()
+
+# KeyError
+# a_dictionary = {
+#     "key": "value"
+# }
+# value = a_dictionary["non_existing_key"]
+
+# IndexError
+# fruit_list = ["banana", "apple", "pear"]
+# fruit = fruit_list[3]
+
+# TypeError
+# text = "abc"
+# print(text+5)
