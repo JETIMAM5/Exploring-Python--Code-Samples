29643920f88ef8c355badeeb13300edfcb168f82
JETIMAM <127851941+JETIMAM5@users.noreply.github.com>
2024-09-07 19:35:16 +0300

    Refactored : Password Generator App ; Handled FileNotFoundError & JSONDecodeError

--- Tkinter/PasswordManagerApp/main.py
+++ Tkinter/PasswordManagerApp/main.py
@@ -37,15 +37,18 @@ def save():
     if len(website) == 0 or len(password) == 0:
         messagebox.showinfo(title="Oops", message="Please make sure you haven't left any fields empty.")
     else:
-        with open("data.json", "r") as data_file:
-            # Reading the old data
-            data = json.load(data_file)
-            # Updating old data with new data
-            data.update(new_data)
-        with open("data.json", "w") as data_file:
-            # Saving Updated data
-            json.dump(data, data_file, indent=4)
-            print(data)
+        try:
+            with open("data.json", "r") as data_file:
+                # Reading the old data
+                data = json.load(data_file)
+                # Updating old data with new data
+                data.update(new_data)
+        except (FileNotFoundError, json.decoder.JSONDecodeError):
+            data = new_data
+        finally:
+            with open("data.json", "w") as data_file:
+                # Saving Updated data
+                json.dump(data, data_file, indent=4)
             website_entry.delete(0, END)
             password_entry.delete(0, END)
 
